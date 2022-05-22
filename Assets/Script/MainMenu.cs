18a5ef47022dc45c7f22ba1ec4ac991e4f703bfd
alexisBes <alexis.bessonnat@epitech.eu>
2022-05-22 17:14:54 +0200

    [Add]menu link to levelone [Add] pause menu minor fix

--- Assets/Script/MainMenu.cs
+++ Assets/Script/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadSceneAsync("mapOne");
+        SceneManager.LoadSceneAsync("LevelOne");
     }
 
     public void ExitGame()
